4e3167986888a610a82e485e5ef88e34946b4ecc
SymbolixAU <dcooley@symbolix.com.au>
2016-04-22 21:26:31 +1000

    updated bind_mongolitedt documentation

--- R/bind_mongolitedt.R
+++ R/bind_mongolitedt.R
@@ -1,11 +1,32 @@
 #' Bind Mongolitedt
 #'
-#' Binds functions to a **mongolite::mongo** object
+#' Binds functions to a \code{mongolite::mongo} object. The data returned from the MongoDB query
+#' must be able to be coerced into a data.table.
+#'
 #' @param mongo A mongolite::mongo connection object to which the functions are bound
 #' @return mongo The mongo object with additional functions
 #' @importFrom Rcpp evalCpp
 #' @useDynLib mongolitedt
 #' @export
+#' @examples
+#' \dontrun{
+#' ## create mongolite::mongo connection
+#' mongo <- mongolite::mongo(collection = "test", db = "test", url = "mongodb://localhost")
+#'
+#' ## bind mongolitedt functions
+#' bind_mongolitedt(mongo)
+#'
+#' ## finddt
+#' mongo$finddt()
+#'
+#' ## aggregatedt
+#' mongo$aggregatedt()
+#' }
+#' @section Methods:
+#' \describe{
+#'  \item{\code{aggregatedt(pipeline = '{}', pagesize = 1000)}}{Execute a pipeline using the Mongo aggregation framework. Will return all data as a single data.table}
+#'  \item{\code{finddt(query = '{}', fields = '{"_id" : 0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000)}}{Retrieve \code{fields} from records matching \code{query}. Will return all data as a single data.table}
+#'  }
 bind_mongolitedt <- function(mongo){
 
   ## test mongo is correct type/class/object
