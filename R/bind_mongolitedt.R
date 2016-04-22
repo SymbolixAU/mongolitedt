c691af0c88dc926340530a40ac5d8eb3dc2244d7
tospig <david.cooley141@gmail.com>
2016-04-23 07:11:52 +1000

    had to add mongolitedt::: prior to stream_in_dt calls. I don't know why.

--- R/bind_mongolitedt.R
+++ R/bind_mongolitedt.R
@@ -16,12 +16,12 @@ bind_mongolitedt <- function(mongo){
   ## assign new finddt function
   mongo$finddt <- function(query = '{}', fields = '{"_id":0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000){
     cur <- mongo_collection_find(col, query = query, sort = sort, fields = fields, skip = skip, limit = limit)
-    mongo_stream_in_dt(cur, pagesize = pagesize, verbose = verbose)
+    mongolitedt:::mongo_stream_in_dt(cur, pagesize = pagesize, verbose = verbose)
   }
 
   mongo$aggregatedt <- function(pipeline = '{}', pagesize = 1000){
     cur <- mongo_collection_aggregate(col, pipeline)
-    mongo_stream_in_dt(cur, pagesize = pagesize, verbose = verbose)
+    mongolitedt:::mongo_stream_in_dt(cur, pagesize = pagesize, verbose = verbose)
   }
 
   environment(mongo$finddt) <- environment(mongo$find)
