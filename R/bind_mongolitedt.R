c26e647b21194544294394ad0f5ec431f57f2568
SymbolixAU <dcooley@symbolix.com.au>
2016-04-24 13:06:06 +1000

    building from the ground up

--- R/bind_mongolitedt.R
+++ R/bind_mongolitedt.R
@@ -1,23 +1,55 @@
+#' Bind Mongolitedt
+#'
+#' Binds methods to a \code{mongolite::mongo} object. The data returned from the MongoDB query
+#' must be able to be coerced into a \code{data.table}.
+#'
+#' @param mongo A \code{mongolite::mongo} connection object to which the methods are bound
+#' @return mongo The mongo object with additional methods
+#' @import mongolite
+#' @importFrom Rcpp evalCpp
+#' @importFrom data.table rbindlist
+#' @useDynLib mongolitedt
+#' @aliases mongolitedt
+#' @examples
+#' \dontrun{
+#' ## create mongolite::mongo connection
+#' mongo <- mongolite::mongo(collection = "test", db = "test", url = "mongodb://localhost")
+#'
+#' ## bind mongolitedt methods
+#' bind_mongolitedt(mongo)
+#'
+#' ## finddt
+#' mongo$finddt()
+#'
+#' ## aggregatedt
+#' mongo$aggregatedt()
+#' }
+#' @section Methods:
+#' \describe{
+#'  \item{\code{aggregatedt(pipeline = '{}', pagesize = 1000)}}{Execute a pipeline using the Mongo aggregation framework. Will return all data as a single data.table}
+#'  \item{\code{finddt(query = '{}', fields = '{"_id" : 0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000)}}{Retrieve \code{fields} from records matching \code{query}. Will return all data as a single data.table}
+#'  }
 #' @export
-bind_mongolitedt <- function(){
+bind_mongolitedt <- function(mongo){
 
   ## test mongo is correct type/class/object
   ## sum(class(mongo) %in% c("mongo", "jeroen")) >= 2
   ## inherits(mongo, "mongo")
   ## is(mongo, "mongo")
-#  unlock_environment(mongo)
+  unlock_environment(mongo)
 
   ## assign new finddt function
-#  mongo$finddt <- function(query = '{}', fields = '{"_id":0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000){
+  mongo$finddt <- function(query = '{}', fields = '{"_id":0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000){
     ##cur <- mongolite:::mongo_collection_find(col, query = query, sort = sort, fields = fields, skip = skip, limit = limit)
     mongo_stream_in_dt()
-#  }
+  }
 
 #   mongo$aggregatedt <- function(pipeline = '{}', pagesize = 1000){
 #     cur <- mongolite:::mongo_collection_aggregate(col, pipeline)
 #     mongolitedt:::mongo_stream_in_dt(cur, pagesize = pagesize, verbose = verbose)
 #   }
 
+    print("exported function\n")
   # environment(mongo$finddt) <- environment(mongo$find)
   # environment(mongo$aggregatedt) <- environment(mongo$find)
 }
--- R/mongo_stream_in_dt.R
+++ R/mongo_stream_in_dt.R
@@ -1,5 +1,41 @@
 mongo_stream_in_dt <- function(){
 
+  # Type validation
+  stopifnot(is.numeric(pagesize))
+  stopifnot(is.logical(verbose))
+
+  # Read data page by page and rbindlist into data.table
+  count <- 0
+  dt <- data.table::data.table()
+  repeat {
+    page <- mongolite:::mongo_cursor_next_page(cur, pagesize)
+    ## page is a list, with a 'date' flag on dates
+    count <- count + length(page)
+
+    ## remove any 'raw' columns (e.g., the '_id' oid field)
+    page <- lapply(page, function(x) x[lapply(x, class) != "raw"])
+
+    dt_page <- try_rbind_page(page)
+    classes <- lapply(dt_page, class) == "list"
+    cols <- names(classes[classes])
+    for(j in cols) set(dt_page, j = j, value = unlist(dt_page[[j]]))
+
+    if(nrow(dt) == 0){
+      dt <- dt_page
+    }else{
+      dt <- data.table::rbindlist(list(dt, dt_page), use.names=T, fill = T)
+    }
+
+    if(verbose)
+      cat("\r Found", count, "records...")
+
+    if(length(page) < pagesize)
+      break
+  }
+
+  if(verbose) cat("\r Imported", count, "records.\n")
+  # return(dt)
+
   print("internal function")
 
 }
