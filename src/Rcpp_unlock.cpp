8ceb49ea32616032fb0417b472e6442399e655cb
SymbolixAU <dcooley@symbolix.com.au>
2016-04-24 19:36:16 +1000

    added link to SO answer showing unlock environment

--- src/Rcpp_unlock.cpp
+++ src/Rcpp_unlock.cpp
@@ -6,6 +6,7 @@ using namespace Rcpp;
 /* This is taken from envir.c in the R 2.15.1 source
 https://github.com/SurajGupta/r-source/blob/master/src/main/envir.c
 */
+// and used in this http://stackoverflow.com/a/25922051/5977215 answer
 #define FRAME_LOCK_MASK (1<<14)
 #define FRAME_IS_LOCKED(e) (ENVFLAGS(e) & FRAME_LOCK_MASK)
 #define UNLOCK_FRAME(e) SET_ENVFLAGS(e, ENVFLAGS(e) & (~ FRAME_LOCK_MASK))
