ac25c135f6669dfe84f7d4069cfed046e410e50a
tospig <david.cooley141@gmail.com>
2016-06-19 17:42:55 +1000

    removed examples - causes error in valgrind checks

--- R/bind_mongolitedt.R
+++ R/bind_mongolitedt.R
@@ -10,25 +10,11 @@
 #' @importFrom data.table rbindlist
 #' @export
 #' @aliases mongolitedt
-#' @examples
-#' \dontrun{
-#' ## create mongolite::mongo connection
-#' mongo <- mongolite::mongo(collection = "test", db = "test", url = "mongodb://localhost")
-#'
-#' ## bind mongolitedt methods
-#' bind_mongolitedt(mongo)
-#'
-#' ## finddt
-#' mongo$finddt()
-#'
-#' ## aggregatedt
-#' mongo$aggregatedt()
-#' }
 #' @section Methods:
 #' \describe{
 #'  \item{\code{aggregatedt(pipeline = '{}', pagesize = 1000)}}{Execute a pipeline using the Mongo aggregation framework. Will return all data as a single data.table}
 #'  \item{\code{finddt(query = '{}', fields = '{"_id" : 0}', sort = '{}', skip = 0, limit = 0, pagesize = 1000)}}{Retrieve \code{fields} from records matching \code{query}. Will return all data as a single data.table}
-#'  }
+#' }
 bind_mongolitedt <- function(mongo){
 
   ## test mongo is correct type/class/object
