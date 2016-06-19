835fc50eea3162958778cbe7e17823ae6529b206
SymbolixAU <dcooley@symbolix.com.au>
2016-06-19 18:19:56 +1000

    getting ready for CRAN

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
 
-  if (interactive()) {
-    packageStartupMessage('mongolitedt is still being developed and is not yet on CRAN.')
-    packageStartupMessage('')
-  }
+  # if (interactive()) {
+  #   packageStartupMessage('mongolitedt is still being developed and is not yet on CRAN.')
+  #   packageStartupMessage('')
+  # }
 
 }
